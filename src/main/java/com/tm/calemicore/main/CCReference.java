538f603f463e747e0ab49df1030f16705bbed44f
Calemi <lemzsteamerson@gmail.com>
2022-01-27 09:33:03 -0500

    Initial commit

---
+++ src/main/java/com/tm/calemicore/main/CCReference.java
@@ -0,0 +1,11 @@
+package com.tm.calemicore.main;
+
+/**
+ * A static reference class used to store common public values.
+ * ex. the mod's name & version.
+ */
+public class CCReference {
+
+    public static final String MOD_ID = "calemicore";
+    public static final String MOD_NAME = "Calemi Core";
+}
---
+++ src/main/java/com/tm/calemicore/main/CalemiCore.java
@@ -0,0 +1,9 @@
+package com.tm.calemicore.main;
+
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * The main class for Calemi Core
+ */
+@Mod(CCReference.MOD_ID)
+public class CalemiCore {}
