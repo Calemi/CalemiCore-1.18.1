220b001b8b2b69528c22eca5ca68a4e0ecf1583e
Calemi <lemzsteamerson@gmail.com>
2022-05-02 20:11:46 -0400

    Updated mc & forge version

--- src/main/java/com/tm/calemicore/util/helper/WorldGenHelper.java
+++ src/main/java/com/tm/calemicore/util/helper/WorldGenHelper.java
@@ -1,5 +1,6 @@
 package com.tm.calemicore.util.helper;
 
+import net.minecraft.core.Holder;
 import net.minecraft.data.worldgen.features.FeatureUtils;
 import net.minecraft.data.worldgen.features.OreFeatures;
 import net.minecraft.data.worldgen.placement.PlacementUtils;
@@ -26,9 +27,9 @@ import java.util.List;
 @Mod.EventBusSubscriber
 public class WorldGenHelper {
 
-    public static final ArrayList<PlacedFeature> oresOverworld = new ArrayList<>();
-    public static final ArrayList<PlacedFeature> oresNether = new ArrayList<>();
-    public static final ArrayList<PlacedFeature> oresEnd = new ArrayList<>();
+    public static final ArrayList<Holder<PlacedFeature>> oresOverworld = new ArrayList<>();
+    public static final ArrayList<Holder<PlacedFeature>> oresNether = new ArrayList<>();
+    public static final ArrayList<Holder<PlacedFeature>> oresEnd = new ArrayList<>();
 
     /**
      * Creates and registers an ore.
@@ -47,17 +48,16 @@ public class WorldGenHelper {
                 OreConfiguration.target(OreFeatures.STONE_ORE_REPLACEABLES, oreBlock.defaultBlockState()),
                 OreConfiguration.target(OreFeatures.DEEPSLATE_ORE_REPLACEABLES, deepslateBlock.defaultBlockState()));
 
-        ConfiguredFeature<?, ?> ORE_FEATURE = FeatureUtils
-                .register(name, Feature.ORE.configured(new OreConfiguration(ORE_TARGET_LIST, veinSize)));
+        Holder<ConfiguredFeature<OreConfiguration, ?>> ORE_FEATURE = FeatureUtils.register(name, Feature.ORE, new OreConfiguration(ORE_TARGET_LIST, veinSize));
 
         List<PlacementModifier> placementModifiers = List.of(
                 CountPlacement.of(veinsPerChunk),
                 InSquarePlacement.spread(),
                 HeightRangePlacement.uniform(VerticalAnchor.absolute(minY), VerticalAnchor.absolute(maxY)), BiomeFilter.biome());
 
-        PlacedFeature ORE_PLACED = PlacementUtils.register("ore_" + name, ORE_FEATURE.placed(placementModifiers));
+        Holder<PlacedFeature> ORE_PLACED = PlacementUtils.register("ore_" + name, ORE_FEATURE, placementModifiers);
 
-        oreList.add(ORE_PLACED);
+        oreList.add(ORE_PLACED.value());
     }
 
     @SubscribeEvent(priority = EventPriority.HIGHEST)
@@ -67,22 +67,22 @@ public class WorldGenHelper {
 
         if (!event.getCategory().equals(Biome.BiomeCategory.NETHER) && !event.getCategory().equals(Biome.BiomeCategory.THEEND)) {
 
-            for (PlacedFeature ore : oresOverworld) {
-                if (ore != null) event.getGeneration().getFeatures(GenerationStep.Decoration.UNDERGROUND_ORES).add(() -> ore);
+            for (Holder<PlacedFeature> ore : oresOverworld) {
+                if (ore != null) event.getGeneration().getFeatures(GenerationStep.Decoration.UNDERGROUND_ORES).add(ore);
             }
         }
 
         else if (event.getCategory().equals(Biome.BiomeCategory.NETHER)) {
 
-            for (PlacedFeature ore : oresNether) {
-                if (ore != null) event.getGeneration().getFeatures(GenerationStep.Decoration.UNDERGROUND_ORES).add(() -> ore);
+            for (Holder<PlacedFeature> ore : oresNether) {
+                if (ore != null) event.getGeneration().getFeatures(GenerationStep.Decoration.UNDERGROUND_ORES).add(ore);
             }
         }
 
         else if (event.getCategory().equals(Biome.BiomeCategory.THEEND)) {
 
-            for (PlacedFeature ore : oresEnd) {
-                if (ore != null) event.getGeneration().getFeatures(GenerationStep.Decoration.UNDERGROUND_ORES).add(() -> ore);
+            for (Holder<PlacedFeature> ore : oresEnd) {
+                if (ore != null) event.getGeneration().getFeatures(GenerationStep.Decoration.UNDERGROUND_ORES).add(ore);
             }
         }
     }
